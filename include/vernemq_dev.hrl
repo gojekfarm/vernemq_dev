0323e0ff8b54ebe85e6a9866d8bc1b2f8b1351b1
Ken Kalang Al Qalyubi <kenkalang@yahoo.com>
2026-02-24 17:32:38 +0700

    feat: enable on_register_failed on hook_name (#13)

--- include/vernemq_dev.hrl
+++ include/vernemq_dev.hrl
@@ -20,7 +20,8 @@
 -type hook_name() :: 'on_register' | 'on_register_m5' | 'auth_on_publish' | 'auth_on_publish_m5' | 'auth_on_register' | 'auth_on_register_m5' 
 | 'auth_on_subscribe' | 'auth_on_subscribe_m5' | 'on_auth_m5' | 'on_deliver' | 'on_deliver_m5' | 'on_unsubscribe' 
 | 'on_unsubscribe_m5' | 'on_publish' | 'on_publish_m5' | 'on_subscribe' | 'on_subscribe_m5' | 'on_message_drop' | 'on_topic_unsubscribed' | 
-'on_session_expired' | 'on_offline_message' | 'on_config_change' | 'on_client_wakeup' | 'on_client_offline' | 'on_client_gone' | 'on_delivery_complete'.
+'on_session_expired' | 'on_offline_message' | 'on_config_change' | 'on_client_wakeup' | 'on_client_offline' | 'on_client_gone' | 'on_delivery_complete'
+| 'on_register_failed'.
 -type matched_acl() :: {matched_acl, Name :: binary() | undefined, Pattern :: binary() | undefined}.
 
 %% reason codes names
