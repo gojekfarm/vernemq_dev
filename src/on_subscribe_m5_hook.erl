6627699ae479f304d8100f3deae12c3eaf2c7b9a
Ken Kalang <kenkalang@yahoo.com>
2026-06-19 14:01:05 +0700

    feat: add matched acl on on_subscribe_m5

--- src/on_subscribe_m5_hook.erl
+++ src/on_subscribe_m5_hook.erl
@@ -5,6 +5,6 @@
 %% called as an 'all'-hook, return value is ignored
 -callback on_subscribe_m5(UserName      :: username(),
                           SubscriberId  :: subscriber_id(),
-                          Topics        :: [{Topic :: topic(), SubInfo :: subinfo()}],
+                          Topics        :: [{Topic :: topic(), SubInfo :: subinfo(), MatchedAcl :: matched_acl()}],
                           Props         :: properties(),
                           SessionId     :: session_id()) -> any().
